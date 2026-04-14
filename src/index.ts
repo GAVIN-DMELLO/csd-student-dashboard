2b5e0369a3643ef2103844c538fe0b0a5cd8fa66
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-04-15 01:39:29 +0530

    basic express backend with prisma to connect to a postgresql db hosted on neondb

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import prisma from './prisma';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+app.post('/students' , async(req , res)=>{
+  try{
+    const student = await prisma.student.createMany(
+      {
+        data:req.body,
+        skipDuplicates: true
+      }
+    )
+    res.status(200).json(
+      {
+        message:"Students created",
+        count: student.count
+      }
+    )
+  }catch(error){
+    res.status(500).json({error:(error as Error).message});
+  }
+});
+
+
+app.delete('/students' , async(req , res)=>{
+  try{
+    const result = await prisma.student.deleteMany({});
+    res.status(200).json(
+      {
+        message:"Students deleted succesfully",
+        count: result.count
+      }
+    )
+  }catch(error){
+    res.status(500).json({error:(error as Error).message});
+  }
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log('✅ Prisma + PostgreSQL connected successfully!');
+});
