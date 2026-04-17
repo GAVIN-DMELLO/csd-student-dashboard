f879db7cfb830d93a8ea88e89d74d09a4ffd8228
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-04-17 23:49:24 +0530

    1.added display cards nto yet styled properly

--- frontend/app/components/AnalysisCards.tsx
+++ frontend/app/components/AnalysisCards.tsx
@@ -1,39 +1,20 @@
-export function AnalysisCard1() {
+import { LucideIcon } from 'lucide-react';
+
+type AnalyticProps = {
+  measuringMetric: string;
+  metricValue: number;
+  className?: string; 
+  Icon: LucideIcon;
+}
+
+export default function AnalysisCard({measuringMetric , metricValue , className , Icon}:AnalyticProps) {
   return (
-    <div>
-      <img src="/icon.png" alt="icon" />
-      <p>Transfer via Card number</p>
-      <span>$1200</span>
+    <div className={`${className}`}>
+      <Icon className="w-6 h-6 text-white" />
+      <p className="text-[rgba(86,87,105,1)]">{measuringMetric}</p>
+      <span className="text-[rgba(253,253,255,1)]">{metricValue}</span>
     </div>
   );
 }
 
-export function AnalysisCard2() {
-  return (
-    <div>
-      <img src="/icon.png" alt="icon" />
-      <p>Transfer via Card number</p>
-      <span>$1200</span>
-    </div>
-  );
-}
 
-export function AnalysisCard3() {
-  return (
-    <div>
-      <img src="/icon.png" alt="icon" />
-      <p>Transfer via Card number</p>
-      <span>$1200</span>
-    </div>
-  );
-}
-
-export function AnalysisCard4() {
-  return (
-    <div>
-      <img src="/icon.png" alt="icon" />
-      <p>Transfer via Card number</p>
-      <span>$1200</span>
-    </div>
-  );
-}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 
+import AnalysisCard from './components/AnalysisCards';
+import { Users, GraduationCap, Award, BookOpen } from 'lucide-react';
+
 export default function Home() {
   return (
     <>
       <div className="grid grid-cols-[150px_1fr_300px] gap-4">
         <aside className="bg-red-500 min-h-screen">sidebar</aside>
-        <main className="bg-red-500">
+        <main>
           <header className="flex justify-between">
             <h1>
               <span className="block text-3xl font-bold text-white">Computer</span>
@@ -13,6 +16,40 @@ export default function Home() {
             </h1>
             <input type="text" placeholder="search" className="bg-[rgba(23,24,33,1)] text-white placeholder-white w-70 h-10 rounded-2xl pl-7 "/>
           </header>
+
+          <section className="flex gap-4 ml-4">        
+            
+            <AnalysisCard 
+              measuringMetric="Class Strength" 
+              metricValue={47} 
+              className="w-60 h-42 rounded-3xl border border-white/5 bg-[radial-gradient(circle_at_top_left,#282c4d,#21222d,#21222d)] shadow-2xl text-[rgba(86,87,105,1)]
+              " 
+              Icon={Users}
+            />
+
+
+            <AnalysisCard 
+              measuringMetric="Class Average Marks" 
+              metricValue={0} 
+              className="w-60 h-42 rounded-3xl border border-white/5 bg-[radial-gradient(circle_at_top_left,#282c4d,#21222d,#21222d)] shadow-2xl text-[rgba(86,87,105,1)]" 
+              Icon={Users}
+            />
+
+            <AnalysisCard 
+              measuringMetric="Total Students" 
+              metricValue={0} 
+              className="w-60 h-42 rounded-3xl border border-white/5 bg-[radial-gradient(circle_at_top_left,#282c4d,#21222d,#21222d)] shadow-2xl text-[rgba(86,87,105,1)]" 
+              Icon={Users}
+            />
+
+            <AnalysisCard 
+              measuringMetric="Attendance Rate" 
+              metricValue={0} 
+              className="w-60 h-42 rounded-3xl border border-white/5 bg-[radial-gradient(circle_at_top_left,#282c4d,#21222d,#21222d)] shadow-2xl text-[rgba(86,87,105,1)]" 
+              Icon={Users}
+            />
+
+          </section>
         </main>
         <aside className="bg-red-500">right panel</aside>
       </div>
