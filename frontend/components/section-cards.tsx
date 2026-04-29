a6e44e547eed98263ac27126b6f45a1fc93a42aa
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-04-29 22:50:56 +0530

    updates in the first card

--- frontend/components/section-cards.tsx
+++ frontend/components/section-cards.tsx
@@ -25,7 +25,7 @@ export function SectionCards() {
     <div className="grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card">
       <Card className="@container/card">
         <CardHeader>
-          <CardDescription>Total Revenue</CardDescription>
+          <CardDescription>Number Of Students</CardDescription>
           <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
             {totalStudents}
           </CardTitle>
@@ -33,7 +33,7 @@ export function SectionCards() {
             <Badge variant="outline">
               <TrendingUpIcon
               />
-              +12.5%
+              +0.0%
             </Badge>
           </CardAction>
         </CardHeader>
